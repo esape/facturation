b060651b05d7f5d2695bf9d517c7bb0f845b1aae
Etienne Guerlain <etienneguerlain@Etiennes-MacBook-Air.local>
2020-02-10 17:43:37 +0100

    Updates

---
+++ precache-manifest.3547f4bc7b8c07833cd0e1ee74178291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88042d198936866b48606c102b0c8fe5",
+    "url": "/facturation/index.html"
+  },
+  {
+    "revision": "fc45b469037f972c0165",
+    "url": "/facturation/static/css/2.c4d1b98b.chunk.css"
+  },
+  {
+    "revision": "f30122463c42d5bb9a6c",
+    "url": "/facturation/static/css/main.e67e969f.chunk.css"
+  },
+  {
+    "revision": "fc45b469037f972c0165",
+    "url": "/facturation/static/js/2.60379346.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/facturation/static/js/2.60379346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30122463c42d5bb9a6c",
+    "url": "/facturation/static/js/main.33f8bcae.chunk.js"
+  },
+  {
+    "revision": "f1b89fe935ddcf4822b2",
+    "url": "/facturation/static/js/runtime-main.95407e10.js"
+  }
+]);
